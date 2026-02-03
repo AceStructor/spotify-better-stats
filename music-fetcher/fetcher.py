3a308123aec4a89d5a3828d0ab13e7c549c49b98
Patrick Ohlsen <patrick.ohlsen@yahoo.com>
2026-02-03 14:32:24 +0100

    Enabled beets again

--- music-fetcher/fetcher.py
+++ music-fetcher/fetcher.py
@@ -61,33 +61,20 @@ class BeetsWorker:
     def __init__(self, track: Track):
         self.track = track 
 
-    def _build_final_path(self) -> str:
-        safe_artist = self.track.artist.replace("/", "_")
-        safe_album = self.track.album.replace("/", "_")
-        filename = f"{self.track.title}.{YTDLP_FORMAT}"
-        return os.path.join(MUSIC_LIBRARY_DIR, safe_artist, safe_album, filename)
-
     def run(self, file_path: str) -> str:
-        final_path = self._build_final_path()
         beets_cmd = [
             "beet",
             "import",
-            "-c",
-            "/beets/beets_config.yaml",
-            "--copy",
-            "--quiet",
             file_path,
         ]
         log.debug(f"Running beets command: {' '.join(beets_cmd)}")
         proc = subprocess.run(beets_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         if proc.returncode != 0:
             raise RuntimeError(proc.stderr)
-        return final_path
-        # Assuming beets moves the file to its library, we need to find the new path
-        # This is a placeholder; actual implementation may vary
-        # final_path = file_path.replace(BEETS_IMPORT_DIR, MUSIC_LIBRARY_DIR).replace("%(ext)s", YTDLP_FORMAT)
-        # return final_path
-
+        path = subprocess.run(f"beet list title:{self.track.title}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if path.returncode != 0:
+            raise RuntimeError(path.stderr)
+        return path.stdout.strip()
 
 class DatabaseWriter:
 
@@ -184,8 +171,9 @@ def worker_loop(worker_id):
                 print(f"[worker-{worker_id}] downloading {track_id}")
 
                 path = YtdlpWorker().run(track)
-                #final_path = BeetsWorker().run(path)
-                #db_writer.mark_done(track, final_path)
+                final_path = BeetsWorker().run(path)
+                log.debug(f"[worker-{worker_id}] imported to beets: {final_path}")
+                db_writer.mark_done(track, final_path)
 
                 print(f"[worker-{worker_id}] done {track_id}")
 
